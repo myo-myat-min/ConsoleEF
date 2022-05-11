3663888c4dc07d5b6eaae0173652eb8453961eab
Myo Myat Min <myomyatmin01@gmail.com>
2022-05-11 12:53:58 +0630

    add two commands in console

--- ConsoleEF/Program.cs
+++ ConsoleEF/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,42 @@ namespace ConsoleEF
     {
         static void Main(string[] args)
         {
+            string[] commands = { "Show devices", "Create a device" };
+
+            Console.WriteLine("Valid Commands");
+
+            for(int i = 0; i < commands.Length; i++)
+            {
+                Console.WriteLine($" {i+1}.{commands[i]}");
+            }
+
+            Console.WriteLine("Please enter a command number");
+            string commandNum = Console.ReadLine();
+
+            switch (commandNum)
+            {
+                case "1":
+                    using (var salesDb = new SalesDBContext())
+                    {
+                        var devices = salesDb.Devices.SqlQuery("SELECT * FROM Devices").ToList();
+                        foreach (var device in devices)
+                        {
+                            Console.WriteLine(device.DeviceName);
+                        }
+                    }
+                    break;
+                case "2":
+                    using(var salesDb = new SalesDBContext())
+                    {
+                        Console.Write("Device Name: ");
+                        string deviceName = Console.ReadLine();
+                        string insertDevice = $"INSERT INTO dbo.Devices VALUES ('{deviceName}');";
+
+                        salesDb.Database.ExecuteSqlCommand(insertDevice);
+                    }
+                    break;
+            }
+
             Console.ReadKey();
         }
     }
